19ea8ade0803501c009b445fb0ea88255b152d11
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-05-31 15:23:02 -0400

    identity check logic for getUser and getUserTasks

--- server/routes/authentication.ts
+++ server/routes/authentication.ts
@@ -32,21 +32,29 @@ router.get(
     checkJwt,
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            console.log(req);
-            const parsedId = req.query.id.toString();
-            const user = await db.User.findOne({
-                where: {
-                    id: parseInt(parsedId),
-                },
-                attributes: ['firstName', 'lastName', 'email'],
-                include: [
-                    {
-                        model: db.Church,
-                        attributes: ['name'],
+            const requestUser = (req.user as any).sub;
+            const requestId = requestUser.split('|')[1];
+            console.log(typeof requestId, requestId);
+            console.log(typeof req.query.id, req.query.id);
+            if (requestId === req.query.id) {
+                console.log('Success');
+                const parsedId = req.query.id.toString();
+                const user = await db.User.findOne({
+                    where: {
+                        id: parseInt(parsedId),
                     },
-                ],
-            });
-            res.json(user);
+                    attributes: ['firstName', 'lastName', 'email'],
+                    include: [
+                        {
+                            model: db.Church,
+                            attributes: ['name'],
+                        },
+                    ],
+                });
+                res.json(user);
+            } else {
+                res.status(404);
+            }
         } catch (err) {
             next(err);
         }
@@ -58,31 +66,37 @@ router.get(
     checkJwt,
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const tasks = await db.Task.findAll({
-                where: {
-                    UserId: req.query.id.toString(),
-                    date: {
-                        [Op.between]: [
-                            '2020-03-07T00:00:00.000Z',
-                            '2020-07-30T00:00:00.000Z',
-                        ],
+            const requestUser = (req.user as any).sub;
+            const requestId = requestUser.split('|')[1];
+            if (requestId === req.query.id) {
+                const tasks = await db.Task.findAll({
+                    where: {
+                        UserId: req.query.id.toString(),
+                        date: {
+                            [Op.between]: [
+                                '2020-03-07T00:00:00.000Z',
+                                '2020-07-30T00:00:00.000Z',
+                            ],
+                        },
                     },
-                },
-                attributes: ['date'],
-                include: [
-                    {
-                        model: db.Role,
-                        as: 'role',
-                        attributes: ['name'],
-                    },
-                    {
-                        model: db.Church,
-                        as: 'church',
-                        attributes: ['name'],
-                    },
-                ],
-            });
-            res.json(tasks);
+                    attributes: ['date'],
+                    include: [
+                        {
+                            model: db.Role,
+                            as: 'role',
+                            attributes: ['name'],
+                        },
+                        {
+                            model: db.Church,
+                            as: 'church',
+                            attributes: ['name'],
+                        },
+                    ],
+                });
+                res.json(tasks);
+            } else {
+                res.status(404);
+            }
         } catch (err) {
             next(err);
         }
