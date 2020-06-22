4d40f81cc4760d9fd0c1bd552f894eba67e298ee
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-06-22 11:40:02 -0400

    removing unnecessary modules from auth0

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import session from 'express-session';
-import openid from 'express-openid-connect';
 import db from './db';
-import auth_config from './authConfig';
 
 const port = process.env.PORT || 8080;
 const app: express.Application = express();
@@ -20,13 +18,12 @@ app.use(
     }),
 );
 
-// app.use(openid.auth(auth_config));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get('/', (req: any, res) => {
-    let msg = 'Welcome to this API. ';
+    const msg = 'Welcome to this API. ';
     res.status(200).send({
         message: msg,
     });
