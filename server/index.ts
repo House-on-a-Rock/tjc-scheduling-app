22feed9119f67fe779d02d07b5a5170be2c25a4f
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-07-29 12:11:15 -0400

    added case to create necessary notifications per request

--- server/index.ts
+++ server/index.ts
@@ -35,6 +35,7 @@ app.use('/api', require('./routes/users'));
 app.use('/api', require('./routes/tasks'));
 app.use('/api', require('./routes/swap-requests'));
 app.use('/api', require('./routes/user-roles'));
+app.use('/api', require('./routes/swap-notifications'));
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
--- server/routes/swap-notifications.ts
+++ server/routes/swap-notifications.ts
@@ -36,7 +36,7 @@ router.get(
                     },
                 ],
             });
-            if (swapNotifications) res.status(200).json(swapNotifications);
+            if (swapNotifications.length > 0) res.status(200).json(swapNotifications);
             else res.status(404).send({ message: 'Not found' });
         } catch (err) {
             if (err instanceof TokenExpiredError || err instanceof JsonWebTokenError) {
@@ -54,31 +54,71 @@ router.post(
     async (req: Request, res: Response, next: NextFunction) => {
         try {
             jwt.verify(req.headers.authorization, cert);
+            let message = '';
             const swapRequest = await db.SwapRequest.findOne({
                 where: { id: req.body.requestId },
-                attributes: ['id', 'requesteeUserId', 'type', 'accepted', 'approved'],
+                attributes: [
+                    'id',
+                    'requesteeUserId',
+                    'type',
+                    'accepted',
+                    'approved',
+                    'TaskId',
+                ],
             });
-            const associatedUser = await db.User.findOne({
+            const requesteeUser = await db.User.findOne({
                 where: { id: swapRequest.requesteeUserId },
-                attributes: ['firstName'],
+                attributes: ['id', 'firstName'],
             });
-            if (req.body.notification === 'accept' && swapRequest) {
-                const newNotification = db.SwapNotification.create({
-                    userId: req.body.userId,
-                    message: `Your requested has been accepted by ${associatedUser.firstName}`,
-                    RequestId: req.body.requestId,
-                });
-                res.status(201).json(newNotification);
-            } else if (req.body.notification === 'approve' && swapRequest) {
-                const newNotification = db.SwapNotification.create({
-                    userId: req.body.userId,
-                    message: `Your request to switch with ${associatedUser.firstName} has been approved`,
-                    RequestId: req.body.requestId,
-                });
-                res.status(201).json(newNotification);
-            } else {
-                res.status(400).send({ message: 'Invalid request' });
+            const requestingUser = await db.User.findOne({
+                where: { id: req.body.userId },
+                attributes: ['id', 'firstName', 'ChurchId'],
+            });
+            const localChurchUsers = await db.User.findAll({
+                where: { ChurchId: requestingUser.ChurchId },
+                attributes: ['id', 'firstName'],
+            });
+            if (!swapRequest)
+                return res.status(404).send({ message: 'Swap request not found' });
+            switch (req.body.notification) {
+                case 'accepted':
+                    message = `Your requested has been accepted by ${requesteeUser.firstName}`;
+                    break;
+                case 'approved':
+                    message = `Your request to switch with ${requesteeUser.firstName} has been approved`;
+                    break;
+                case 'cancelled':
+                    message = `Your request to switch has been cancelled`;
+                    break;
+                case 'created':
+                    message = `Request sent to ${requesteeUser.firstName}`;
+                    if (swapRequest.type === 'requestOne') {
+                        await db.SwapNotification.create({
+                            userId: requesteeUser.id,
+                            message: `${requestingUser.firstName} wants to switch with you`,
+                            RequestId: req.body.requestId,
+                        });
+                    } else {
+                        localChurchUsers.map(async (user) => {
+                            await db.SwapNotification.create({
+                                userId: user.id,
+                                message: `${requestingUser.firstName} requested to switch with someone. An open request has been sent out.`,
+                                RequestId: req.body.requestId,
+                            });
+                        });
+                    }
+                    break;
+                default:
+                    return res
+                        .status(400)
+                        .send({ message: 'Invalid notification type.' });
             }
+            await db.SwapNotification.create({
+                userId: req.body.userId,
+                message: message,
+                RequestId: req.body.requestId,
+            });
+            res.status(201).send({ message: 'Notifications created' });
         } catch (err) {
             if (err instanceof TokenExpiredError || err instanceof JsonWebTokenError) {
                 res.status(401).send({ message: 'Unauthorized' });
@@ -89,6 +129,19 @@ router.post(
         }
     },
 );
+
+// router.post('/swap-notifications/receivingUsers', async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+
+//     }catch (err) {
+//         if (err instanceof TokenExpiredError || err instanceof JsonWebTokenError) {
+//             res.status(401).send({ message: 'Unauthorized' });
+//         } else {
+//             res.status(503).send({ message: 'Server error, try again later' });
+//         }
+//         next(err);
+//     }
+// })
 
 router.delete(
     '/swap-notifications/:notificationId',
