b353185f43820cb1b2ebbf3f85b0ffac9f77d1c7
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-07-30 16:40:25 -0400

    uncommenting jwt checks

--- server/routes/swap-requests.ts
+++ server/routes/swap-requests.ts
@@ -15,7 +15,7 @@ module.exports = router;
 
 router.get('/swap-requests', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        // jwt.verify(req.headers.authorization, cert);
+        jwt.verify(req.headers.authorization, cert);
         const searchParams: number[] = [];
         req.query.taskId
             .toString()
