87c0a409bf933ada821d62080c720ad66a50516a
Shaun Tung <stung@datacolor.com>
2020-10-09 16:50:54 -0400

    debug userroles changes in tasks

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -19,30 +19,21 @@ router.get('/tasks', certify, async (req: Request, res: Response, next: NextFunc
 
         const searchParams = {
             [Op.and]: searchArray,
-            date: { [Op.gt]: new Date() },
+            // date: { [Op.gt]: new Date() },
         };
-        const tasks = await db.Task.findAll({
-            where: searchParams,
-            attributes: [['id', 'taskId'], 'userId', 'roleId', 'date', 'createdAt'],
-            include: [
-                {
-                    model: db.Role,
-                    as: 'role',
-                    attributes: ['name'],
-                },
-                {
-                    model: db.Church,
-                    as: 'church',
-                    attributes: ['name', ['id', 'churchId']],
-                },
-                {
-                    model: db.User,
-                    as: 'user',
-                    attributes: ['firstName', 'lastName'],
-                },
-            ],
-            order: [['date', 'ASC']],
-        });
+        const tasks = [];
+        if (userId || roleId) {
+            const userRoles = await db.UserRole.findAll({ where: searchParams });
+            const query = userRoles.map(async ({ id }) => {
+                const task = await db.Task.findOne({ where: { userRoleId: id } });
+                tasks.push(task);
+            });
+            await Promise.all(query);
+        }
+        if (churchId) {
+            const query = await db.Task.findAll({ where: searchParams });
+            tasks.push(...query);
+        }
         return tasks ? res.status(200).json(tasks) : res.status(404).send({ message: 'Not found' });
     } catch (err) {
         next(err);
@@ -53,8 +44,8 @@ router.get('/tasks', certify, async (req: Request, res: Response, next: NextFunc
 router.get('/tasks/:taskId', certify, async (req: Request, res: Response, next: NextFunction) => {
     try {
         const task = await db.Task.findOne({
-            where: { taskId: req.params.taskId },
-            attributes: ['id', 'date', 'churchId', 'userId', 'roleId'],
+            where: { id: req.params.taskId },
+            attributes: ['id', 'date', 'churchId', 'userRoleId'],
         });
         return task ? res.status(200).json(task) : res.status(404).send({ message: 'Task not found' });
     } catch (err) {
@@ -65,24 +56,14 @@ router.get('/tasks/:taskId', certify, async (req: Request, res: Response, next: 
 
 router.post('/tasks', certify, async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { date, time, roleId } = req.body;
-        const userId = jwt.decode(req.headers.authorization, { json: true }).sub.split('|')[1];
-        const { church } = await db.User.findOne({
-            where: { id: userId },
-            include: [
-                {
-                    model: db.Church,
-                    as: 'church',
-                    attributes: ['id', 'name', 'address', 'timezone'],
-                },
-            ],
-        });
-        const taskDate = setDate(date, time, church.timezone);
+        // req.body could have userId + roleId, or userRoleId
+        const { date, time, userRoleId, churchId } = req.body;
+        const { timezone } = await db.Church.findOne({ where: { id: churchId } });
+        const taskDate = setDate(date, time, timezone);
         const task = await db.Task.create({
             date: new Date(taskDate.toString()),
-            churchId: church.id,
-            roleId,
-            userId: parseInt(userId, 10),
+            churchId,
+            userRoleId,
         });
         return res.status(201).send(task);
     } catch (err) {
@@ -97,8 +78,7 @@ router.delete('/tasks/:taskId', certify, async (req: Request, res: Response, nex
             where: { id: req.params.taskId },
         });
         const [message, status] = task ? ['Task deleted', 200] : ['Task not found', 404];
-        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        status === 200 && (await task.destroy());
+        if (status === 200) await task.destroy();
         return res.status(status).send({ message });
     } catch (err) {
         next(err);
@@ -114,12 +94,12 @@ router.patch(
             const { targetTaskId, switchTaskId } = req.params;
             const targetTask = await db.Task.findOne({
                 where: { id: targetTaskId },
-                attributes: ['id', 'date', 'churchId', 'userId', 'roleId'],
+                attributes: ['id', 'date', 'churchId', 'userRoleId'],
             });
 
             const switchTask = await db.Task.findOne({
                 where: { id: switchTaskId },
-                attributes: ['id', 'date', 'churchId', 'userId', 'roleId'],
+                attributes: ['id', 'date', 'churchId', 'userRoleId'],
             });
 
             const [message, status] =
@@ -128,12 +108,12 @@ router.patch(
             if (status === 200) {
                 await targetTask.update({
                     id: targetTask.id,
-                    userId: switchTask.userId,
+                    userRoleId: switchTask.userRoleId,
                 });
 
                 await switchTask.update({
                     id: switchTask.id,
-                    userId: targetTask.userId,
+                    userRoleId: targetTask.userRoleId,
                 });
             }
             return res.status(status).send({ message });
@@ -145,36 +125,35 @@ router.patch(
 );
 
 router.patch(
-    '/tasks/replaceTask/:taskId/replacedBy/:userId',
+    '/tasks/replaceTask/:taskId/replacedBy/:userRoleId',
     certify,
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const { taskId, userId } = req.params;
+            const { taskId, userRoleId } = req.params;
             const task = await db.Task.findOne({
-                where: {
-                    id: taskId,
-                },
-                attributes: ['id', 'userId'],
+                where: { id: taskId },
+                attributes: ['id', 'userRoleId'],
             });
 
-            const replacedByUser = await db.User.findOne({
-                where: { id: userId },
-                attributes: ['id', 'churchId'],
+            const replacingTeammate = await db.UserRole.findOne({
+                where: { id: userRoleId },
+                attributes: ['id'],
             });
 
-            const belongsToUser = await db.User.findOne({
-                where: { id: task.userId },
-                attributes: ['id', 'churchId'],
+            const askingTeammate = await db.UserRole.findOne({
+                where: { id: task.userRoleId },
+                attributes: ['id'],
             });
+
             const [message, status] =
-                !task || !replacedByUser || !belongsToUser
+                !task || !replacingTeammate || !askingTeammate
                     ? ['Task replacement successful', 200]
                     : ['Task not found', 404];
 
             if (status === 200)
                 await task.update({
                     id: task.id,
-                    userId: replacedByUser.id,
+                    userRoleId: replacingTeammate.id,
                 });
             return res.status(status).send({ message });
         } catch (err) {
