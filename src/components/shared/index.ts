28e395771810348bcc0b1ab9fdf20a6b4fd54622
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2020-12-24 20:33:19 -0500

    commenting out error404

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,4 +2,4 @@ export * from './Alert';
 export * from './Copyright';
 export * from './LoadingPage';
 export * from './TransitionsModal';
-export * from './Error404';
+// export * from './Error404';
