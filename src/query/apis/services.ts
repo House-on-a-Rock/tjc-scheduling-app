c4ee2af9db91d094ec5712344a03dfe06d95fccc
Ted <changxin.t.chen@gmail.com>
2021-01-13 15:59:02 -0500

    removed dead code, console.log

--- src/query/apis/services.ts
+++ src/query/apis/services.ts
@@ -4,6 +4,5 @@ import { NewServiceData } from '../../shared/types';
 const secretIp = 'http://127.0.0.1:8081';
 
 export const postService = (serviceInfo: NewServiceData): Promise<AxiosResponse> => {
-  console.log('serviceInfo', serviceInfo);
   return axios.post(`${secretIp}/api/service`, serviceInfo);
 };
