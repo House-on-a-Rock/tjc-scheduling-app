46670dbd3eebf6dd6a9b60492edeb04bdb7ea91d
hchiam <hchiam@users.noreply.github.com>
2021-01-24 22:48:30 -0500

    add tooltip for better UX

--- src/components/Schedule/ScheduleTableBody.tsx
+++ src/components/Schedule/ScheduleTableBody.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { TableCell, TableRow, TableBody } from '@material-ui/core';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import ReactTooltip from 'react-tooltip';
 
 export const ScheduleTableBody = ({ title, children }: any) => {
+  const tooltipId = `${title}_tooltip`;
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   return (
     <>
       <TableBody>
         <TableRow onClick={() => setOpen(!open)}>
-          <TableCell>{title}</TableCell>
+          <TableCell className={classes.scheduleTitle} data-tip data-for={tooltipId}>
+            {title}
+          </TableCell>
+          <ReactTooltip id={tooltipId} type="info">
+            <span>{`Click to ${open ? 'collapse' : 'expand'}`}</span>
+          </ReactTooltip>
         </TableRow>
       </TableBody>
       <TableBody
-        className={`${classes.scheduleTableBody} ${!open && classes.collapsedTableBody}`}
+        className={`${classes.groupOfRows} ${!open && classes.collapsedGroupOfRows}`}
       >
         {children}
       </TableBody>
@@ -23,12 +30,13 @@ export const ScheduleTableBody = ({ title, children }: any) => {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    scheduleTableBody: {
-      transition: 'transform 0.2s, visibility 0.1s',
+    scheduleTitle: {},
+    groupOfRows: {
+      transition: 'transform 0.2s, visibility 0.15s',
       transformOrigin: 'top',
       width: '20ch',
     },
-    collapsedTableBody: {
+    collapsedGroupOfRows: {
       transform: 'scaleY(0)',
       visibility: 'collapse',
       opacity: 0.5,
