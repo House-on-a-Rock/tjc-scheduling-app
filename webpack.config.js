7a7a51312d5ab5553c5360ad90f0a9938e52f7df
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-01-30 16:42:41 -0500

    updating webpack paths

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[hash].js',
-    publicPath: '/',
+    publicPath: 'dist',
   },
   devtool: 'source-map',
   // watch: true,
