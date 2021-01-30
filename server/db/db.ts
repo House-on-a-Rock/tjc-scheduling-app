6bfc6093be694d214ad49392383162a5e4902034
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-01-30 15:20:55 -0500

    resolving deployment run errors

--- server/db/db.ts
+++ server/db/db.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const env = process.env.NODE_ENV ?? 'development';
 
 const config = configJson[env];
-config.logging = false;
+// config.logging = false;
 
 const db = createModels(config.database, config.username, config.password, config);
 
