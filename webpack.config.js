226d79e6468aacace674d11852badcb1095c185a
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-01-30 18:03:00 -0500

    t

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[hash].js',
+    filename: 'bundle.js',
     publicPath: '/',
   },
   devtool: 'source-map',
