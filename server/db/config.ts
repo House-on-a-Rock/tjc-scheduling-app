8c98230331bc00485bff4b8cd8535d8d68caad2c
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-01-30 15:32:30 -0500

    resolving db errors in heroku deploy

--- server/db/config.ts
+++ server/db/config.ts
@@ -22,6 +22,13 @@ const config: ConfigInterface = {
     host: process.env.DB_HOST ?? '',
     dialect: 'postgres',
   },
+  production: {
+    database: process.env.DB_NAME ?? '',
+    username: process.env.DB_USER ?? '',
+    password: process.env.DB_PASS ?? '',
+    host: process.env.DB_HOST ?? '',
+    dialect: 'postgres',
+  },
 };
 
 export default config;
