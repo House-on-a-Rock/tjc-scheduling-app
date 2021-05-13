6173f3c98be8439dad46241b288cbfb1fd3c3de7
Ted <changxin.t.chen@gmail.com>
2021-05-13 01:30:45 -0400

    schedule business logic moved to services folder

--- server/routes/schedules.js
+++ server/routes/schedules.js
@@ -1,29 +1,15 @@
-/* eslint-disable no-case-declarations */
 /* eslint-disable consistent-return */
-/* eslint-disable array-callback-return */
 import express from 'express';
 
-import { daysOfWeek } from '../../shared/constants';
-import db from '../index';
 import {
   createSchedule,
+  deleteSchedule,
   doesScheduleExist,
   retrieveChurchSchedules,
   retrieveOneSchedule,
+  updateSchedule,
 } from '../services/schedules';
-import {
-  certify,
-  createColumns,
-  deleteEvents,
-  deleteServices,
-  populateServiceData,
-  recurringDaysOfWeek,
-  removeTimezoneFromDate,
-  updateEvents,
-  updateServices,
-  updateTasks,
-  weeksRange,
-} from '../utilities/helperFunctions';
+import { certify } from '../utilities/helperFunctions';
 
 const router = express.Router();
 
@@ -54,7 +40,7 @@ router.post('/schedule', certify, async (req, res, next) => {
   try {
     const { churchId, title } = req.body;
     if (await doesScheduleExist(churchId, title)) {
-      // TODO clean up all responses
+      // TODO clean up and make consistent responses
       res.statusMessage = 'Schedule already exists';
       res.status(409).send();
       return;
@@ -72,32 +58,11 @@ router.post('/schedule', certify, async (req, res, next) => {
   }
 });
 
-/*
-  should the backend have error checking to ensure the submitted items are valid??
-  i think it'll be a lot of extra work, and the front end will be handling that already.
-  on the other hand, kinda feels risky not having validation
-*/
-
-const updateRouter = {
-  tasks: updateTasks,
-  services: updateServices,
-  events: updateEvents,
-  deletedServices: deleteServices,
-  deletedEvents: deleteEvents,
-};
-
 // should this route be renamed?
 router.post('/schedule/update', certify, async (req, res, next) => {
   const changes = req.body;
   try {
-    // eslint-disable-next-line no-unused-vars
-    const sequelizeTransaction = db.sequelize.transaction(async (transaction) => {
-      await Promise.all(
-        Object.keys(changes).map(async (type) => {
-          return updateRouter[type](changes[type], transaction);
-        }),
-      );
-    });
+    await updateSchedule(changes);
     return res.status(200).send(`Schedule updated successfully!`);
   } catch (err) {
     next(err);
@@ -108,16 +73,12 @@ router.post('/schedule/update', certify, async (req, res, next) => {
 router.delete('/schedule', certify, async (req, res, next) => {
   try {
     const { scheduleId, title } = req.body;
-    await db.Schedule.destroy({
-      where: {
-        id: scheduleId,
-        title,
-      },
-    });
+    await deleteSchedule(scheduleId, title);
     return res.status(200).send(`Schedule ${title} successfully deleted`);
   } catch (err) {
     next(err);
     return res.status(503).send({ message: 'Server error, try again later' });
   }
 });
+
 export default router;
--- server/services/schedules.js
+++ server/services/schedules.js
@@ -1,20 +1,26 @@
-import express from 'express';
-
 import { daysOfWeek } from '../../shared/constants';
 import db from '../index';
 import {
-  certify,
   createColumns,
-  deleteEvents,
-  deleteServices,
   populateServiceData,
   recurringDaysOfWeek,
   removeTimezoneFromDate,
-  updateEvents,
-  updateServices,
-  updateTasks,
   weeksRange,
 } from '../utilities/helperFunctions';
+
+/*
+  should the backend have error checking to ensure the submitted items are valid??
+  i think it'll be a lot of extra work, and the front end will be handling that already.
+  on the other hand, kinda feels risky not having validation
+*/
+
+const updateRouter = {
+  tasks: updateTasks,
+  services: updateServices,
+  events: updateEvents,
+  deletedServices: deleteServices,
+  deletedEvents: deleteEvents,
+};
 
 export const retrieveChurchSchedules = async (churchId) =>
   db.Schedule.findAll({
@@ -76,6 +82,16 @@ export const createSchedule = async (request) => {
   return newSchedule;
 };
 
+export const updateSchedule = async (changes) => {
+  const sequelizeTransaction = db.sequelize.transaction(async (transaction) => {
+    await Promise.all(
+      Object.keys(changes).map(async (type) => {
+        return updateRouter[type](changes[type], transaction);
+      }),
+    );
+  });
+};
+
 export const deleteSchedule = async (scheduleId, title) => {
   await db.Schedule.destroy({
     where: {
@@ -122,3 +138,102 @@ const constructScheduleByTemplate = async (newSchedule, templateId) => {
     });
   });
 };
+
+async function updateEvents(events, t) {
+  await Promise.all(
+    events.map(async (item, index) => {
+      const { eventId, time, roleId, serviceId } = item;
+      const targetEvent = await db.Event.findOne({
+        where: { id: eventId },
+      });
+
+      if (targetEvent)
+        // if event already exists, update to match incoming data
+        return targetEvent.update({ time, roleId, order: index }, { transaction: t });
+      else {
+        // else create new event, and corresponding tasks
+        const newEvent = await db.Event.create(
+          { time, roleId, serviceId, order: index },
+          { transaction: t },
+        );
+        const parentService = await db.Service.findOne({
+          where: { id: serviceId },
+        });
+        const parentSchedule = await db.Schedule.findOne({
+          where: { id: parentService.scheduleId },
+        });
+        const taskDays = recurringDaysOfWeek(
+          parentSchedule.start,
+          parentSchedule.end,
+          parentService.day,
+        );
+        taskDays.forEach(async (date) =>
+          db.Task.create({ date, eventId: newEvent.id }, { transaction: t }),
+        );
+        return newEvent;
+      }
+    }),
+  );
+}
+
+async function updateServices(services, t) {
+  await Promise.all(
+    services.map(async (item, index) => {
+      const { name, day, scheduleId, serviceId } = item;
+      if (serviceId) {
+        const targetService = await db.Service.findOne({
+          where: { id: serviceId },
+        });
+        return targetService.update(
+          { name: name, day: day, order: index },
+          { transaction: t },
+        );
+      } else {
+        return db.Service.create(
+          { name: name, day: day, scheduleId: scheduleId, order: index },
+          { transaction: t },
+        );
+      }
+    }),
+  );
+}
+
+async function updateTasks(tasks, t) {
+  await Promise.all(
+    tasks.map(async (item) => {
+      const targetTask = await db.Task.findOne({
+        where: { id: item.taskId },
+      });
+      return targetTask.update(
+        { userId: item.userId > 0 ? item.userId : null },
+        { transaction: t },
+      );
+    }),
+  );
+}
+
+async function deleteServices(Services, t) {
+  await Promise.all(
+    Services.map(async (item) => {
+      await db.Service.destroy(
+        {
+          where: { id: item },
+        },
+        { transaction: t },
+      );
+    }),
+  );
+}
+
+async function deleteEvents(events, t) {
+  await Promise.all(
+    events.map(async (item) => {
+      await db.Event.destroy(
+        {
+          where: { id: item },
+        },
+        { transaction: t },
+      );
+    }),
+  );
+}
