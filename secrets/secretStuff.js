071f4c3c3e3fd043fd754c67db90c51a36ad296b
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-07-09 19:06:01 -0400

    including not-so-secret secrets

---
+++ secrets/secretStuff.js
@@ -0,0 +1,15 @@
+const secret_database = {
+  test: {
+    DB_NAME: 'tjc-scheduling',
+    DB_USERNAME: 'postgres',
+    DB_PASSWORD: 'Ps372324',
+    DB_HOST: '127.0.0.1',
+  },
+};
+
+const secretIp = 'http://10.10.150.50:8081';
+
+module.exports = {
+  secret_database,
+  secretIp,
+};
