fd77ca1796b8ba550b312fc8eaa744ebd567890e
Shaun Tung <shaun.tung@gmail.com>
2021-12-09 15:38:51 +0800

    use toggle accepts events

--- dutiful/src/hooks/useToggle.js
+++ dutiful/src/hooks/useToggle.js
@@ -4,8 +4,8 @@ export const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState);
 
   function handleToggle(bool) {
-    if (bool !== undefined) setState(bool);
-    else setState(!state);
+    if (bool === undefined || bool.target) setState(!state);
+    else setState(bool);
   }
 
   return [state, handleToggle];
