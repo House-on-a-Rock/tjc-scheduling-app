7de5f83adf760f30d0d148cb3967484347d66ac1
Shaun Tung <shaun.tung@gmail.com>
2021-12-10 17:36:30 +0800

    outlined and combines classnames correctly

--- dutiful/src/components/chip/Pill.jsx
+++ dutiful/src/components/chip/Pill.jsx
@@ -2,16 +2,34 @@ import { forwardRef } from 'react';
 import { Chip, makeStyles } from '@material-ui/core';
 import clsx from 'clsx';
 
-export const Pill = forwardRef(({ className, ...props }, ref) => {
+export const Pill = forwardRef(({ className = '', ...props }, ref) => {
   const classes = useStyles();
-  return <Chip className={clsx(className, classes.pill)} ref={ref} {...props} />;
+  const isDefault = !className || !props.variant;
+
+  return (
+    <Chip
+      classes={{ root: classes.pill }}
+      className={clsx(classes.root, isDefault && classes.defaultStyle, className)}
+      ref={ref}
+      {...props}
+    />
+  );
 });
 
 const useStyles = makeStyles((theme) => ({
-  pill: {
+  root: {
     borderRadius: theme.spacing(0.5),
     height: 'fit-content',
     paddingTop: theme.spacing(0.25),
     paddingBottom: theme.spacing(0.25),
+  },
+  defaultStyle: {
+    color: 'white',
+    backgroundColor: theme.palette.primary.light,
+  },
+  pill: {
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
 }));
